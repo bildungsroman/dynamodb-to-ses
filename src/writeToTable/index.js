1620e623a75fea463b1e40a97d3a5feac306a3e9
Anna Spysz <anna.spysz@stackery.io>
2019-04-08 15:57:45 -0700

    Adding initial testing code

--- src/writeToTable/index.js
+++ src/writeToTable/index.js
@@ -1,5 +1,73 @@
-exports.handler = async message => {
-  console.log(message);
+require('isomorphic-fetch');
+const AWS = require('aws-sdk');
+const Unsplash = require('unsplash-js').default;
+const toJson = require('unsplash-js').toJson;
+const unsplash = new Unsplash({
+  applicationId: process.env.UNSPLASH_ACCESS_KEY,
+  secret: process.env.UNSPLASH_SECRET_KEY
+});
 
-  return {};
-}
+exports.handler = async () => {
+  // get items from the unsplash api
+  const getPhotos = await unsplash.search.photos('servers');
+
+  const result = toJson(getPhotos);
+
+  // write new items to the ServerTable
+  // duplicate entries will be skipped
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+  // we'll be using promises to make sure everything gets written when the lambda is called
+  let promiseArray = [];
+
+  // for (let item of changelogEntries) {
+  //   const itemId = `${_.camelCase(item.Title)}-${item.Repository}`;
+  //   const params = {
+  //     TableName: process.env.TABLE_NAME,
+  //     Item: {
+  //       id: itemId,
+  //       date: item.date,
+  //       Repository: item.Repository,
+  //       Title: item.Title,
+  //       Author: item.Author,
+  //       Feature: item.Feature || null,
+  //       Resource: item.Resource || null,
+  //       Release: item.Release || null,
+  //       Fix: item.Fix || null,
+  //       Deprecation: item.Deprecation || null,
+  //       Refactor: item.Refactor || null,
+  //       References: item.References || null,
+  //     },
+  //     ConditionExpression: 'attribute_not_exists(id)', // do not overwrite existing entries
+  //     ReturnConsumedCapacity: 'TOTAL'
+  //   };
+
+  //   const p = dynamodb.put(params)
+  //     .promise()
+  //     .then(() => {
+  //       console.log(`Writing ${itemId} to table ${process.env.TABLE_NAME}. Sweet!`);
+  //     })
+  //     .catch((error) => {
+  //       console.log(`Error writing ${itemId} to table ${process.env.TABLE_NAME}. The entry may already exist.`);
+  //       console.log(error.message);
+  //     });
+  //   promiseArray.push(p);
+  // };
+
+  // try {
+  //   await Promise.all(promiseArray);
+  //   console.log('Writing to dynamodb');
+  // } catch (error) {;
+  //   console.log(error);
+  // }
+
+  // return a 200 response if no errors
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(result)
+  };
+
+  return response;
+};
