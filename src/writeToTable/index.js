526e6f12f810ddbe4c9596e015522af2a7262d66
Anna Spysz <anna.spysz@stackery.io>
2019-04-09 13:58:24 -0700

    Added dybamodb logic

--- src/writeToTable/index.js
+++ src/writeToTable/index.js
@@ -3,10 +3,11 @@ const Flickr = require('flickr-sdk');
 
 async function getPhotos () {
   const flickr = new Flickr(process.env.FLICKR_API_KEY);
-  // get items from the flickr api
+  // get photos from the flickr api
   try {
+    // https://www.flickr.com/services/api/flickr.photos.search.html
     const photos = await flickr.photos.search({
-      text: 'servers datacenter',
+      text: 'servers datacenter', // the search text
       sort: 'interestingness-desc', // sort to attempt to reduce spam photos
       content_type: 1, // only photos
       safe_search: 1, // only SFW photos please
@@ -22,54 +23,46 @@ async function getPhotos () {
 }
 
 exports.handler = async () => {
+  // fetch an array of photo objects
   const photoArray = await getPhotos();
-  console.log(photoArray);
   // write new items to the ServerTable
   // duplicate entries will be skipped
   const dynamodb = new AWS.DynamoDB.DocumentClient();
   // we'll be using promises to make sure everything gets written when the lambda is called
   let promiseArray = [];
 
-  // for (let item of changelogEntries) {
-  //   const itemId = `${_.camelCase(item.Title)}-${item.Repository}`;
-  //   const params = {
-  //     TableName: process.env.TABLE_NAME,
-  //     Item: {
-  //       id: itemId,
-  //       date: item.date,
-  //       Repository: item.Repository,
-  //       Title: item.Title,
-  //       Author: item.Author,
-  //       Feature: item.Feature || null,
-  //       Resource: item.Resource || null,
-  //       Release: item.Release || null,
-  //       Fix: item.Fix || null,
-  //       Deprecation: item.Deprecation || null,
-  //       Refactor: item.Refactor || null,
-  //       References: item.References || null,
-  //     },
-  //     ConditionExpression: 'attribute_not_exists(id)', // do not overwrite existing entries
-  //     ReturnConsumedCapacity: 'TOTAL'
-  //   };
+  for (let photo of photoArray) {
+    const params = {
+      TableName: process.env.TABLE_NAME,
+      Item: {
+        id: photo.id,
+        title: photo.title || null,
+        url: photo.url_o,
+        height: photo.height_o,
+        width: photo.width_o
+      },
+      ConditionExpression: 'attribute_not_exists(id)', // do not overwrite existing entries
+      ReturnConsumedCapacity: 'TOTAL'
+    };
 
-  //   const p = dynamodb.put(params)
-  //     .promise()
-  //     .then(() => {
-  //       console.log(`Writing ${itemId} to table ${process.env.TABLE_NAME}. Sweet!`);
-  //     })
-  //     .catch((error) => {
-  //       console.log(`Error writing ${itemId} to table ${process.env.TABLE_NAME}. The entry may already exist.`);
-  //       console.log(error.message);
-  //     });
-  //   promiseArray.push(p);
-  // };
+    const p = dynamodb.put(params)
+      .promise()
+      .then(() => {
+        console.log(`Writing ${photo.id} to table ${process.env.TABLE_NAME}. Sweet!`);
+      })
+      .catch((error) => {
+        console.log(`Error writing ${photo.id} to table ${process.env.TABLE_NAME}. The entry may already exist.`);
+        console.log(error.message);
+      });
+    promiseArray.push(p);
+  };
 
-  // try {
-  //   await Promise.all(promiseArray);
-  //   console.log('Writing to dynamodb');
-  // } catch (error) {;
-  //   console.log(error);
-  // }
+  try {
+    await Promise.all(promiseArray);
+    console.log('Writing to dynamodb');
+  } catch (error) {;
+    console.log(error);
+  }
 
   // return a 200 response if no errors
   const response = {
